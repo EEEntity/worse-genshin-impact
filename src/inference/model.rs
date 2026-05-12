49ac6d2c390dfd6b3adde22eaf0f147b35eab6e5
eeentity-work <eeentity@163.com>
2026-05-12 16:06:49 +0800

    更新模型

--- src/inference/model.rs
+++ src/inference/model.rs
@@ -9,6 +9,22 @@ pub const ORT_LIB_PATH: &str = "/usr/lib/libonnxruntime.so";
 pub const MODEL_PPOCR_DET_V4: &str = "ch_PP-OCRv4_det_mobile.onnx";
 pub const MODEL_PPOCR_REC_V4: &str = "ch_PP-OCRv4_rec_mobile.onnx";
 pub const LABEL_PPOCR_KEYS_V1: &str = "ppocr_keys_v1.txt";
+// 角色侧栏
+pub const MODEL_AVATAR_SIDE: &str = "Common/avatar_side_classify_sim.onnx";
+// Q图标分类
+pub const MODEL_Q_CLASSIFY: &str = "Common/q_classify_sim.onnx";
+// 大世界
+pub const MODEL_WORLD: &str = "World/bgi_world.onnx";
+// 秘境古树
+pub const MODEL_TREE: &str = "Domain/bgi_tree.onnx";
+// 鱼类
+pub const MODEL_FISH: &str = "Fish/bgi_fish.onnx";
+// Yap
+pub const MODEL_YAP: &str = "Yap/model_training.onnx";
+pub const LABEL_YAP: &str = "Yap/index_2_word.json";
+// 物品grid图标分类
+pub const MODEL_GRID_ICON: &str = "Item/gridIcon.onnx";
+pub const LABEL_GRID_ICON: &str = "Item/items.csv";
 
 fn in_models_dir(path: &str) -> String {
     format!("{MODELS_DIR}/{path}")
@@ -19,6 +35,13 @@ fn in_models_dir(path: &str) -> String {
 pub enum Model {
     PaddleOCRDetV4,
     PaddleOCRRecV4,
+    AvatarSide,
+    QClassify,
+    World,
+    Tree,
+    Fish,
+    Yap,
+    GridIcon,
 }
 
 impl Model {
@@ -26,6 +49,13 @@ impl Model {
         match self {
             Model::PaddleOCRDetV4 => MODEL_PPOCR_DET_V4,
             Model::PaddleOCRRecV4 => MODEL_PPOCR_REC_V4,
+            Model::AvatarSide => MODEL_AVATAR_SIDE,
+            Model::QClassify => MODEL_Q_CLASSIFY,
+            Model::World => MODEL_WORLD,
+            Model::Tree => MODEL_TREE,
+            Model::Fish => MODEL_FISH,
+            Model::Yap => MODEL_YAP,
+            Model::GridIcon => MODEL_GRID_ICON,
         }
     }
     /// 模型`.onnx`文件对应路径
@@ -36,6 +66,8 @@ impl Model {
     pub const fn label_path(self) -> Option<&'static str> {
         match self {
             Model::PaddleOCRRecV4 => Some(LABEL_PPOCR_KEYS_V1),
+            Model::Yap => Some(LABEL_YAP),
+            Model::GridIcon => Some(LABEL_GRID_ICON),
             _ => None,
         }
     }
@@ -47,6 +79,13 @@ impl Model {
         match self {
             Model::PaddleOCRDetV4 => "PaddleOCRDetV4",
             Model::PaddleOCRRecV4 => "PaddleOCRRecV4",
+            Model::AvatarSide => "AvatarSide",
+            Model::QClassify => "QClassify",
+            Model::World => "World",
+            Model::Tree => "Tree",
+            Model::Fish => "Fish",
+            Model::Yap => "Yap",
+            Model::GridIcon => "GridIcon",
         }
     }
 }
